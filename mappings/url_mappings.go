bf57bbcccc52f3e69f5ea0416b4cf9d52949de07
satender55tech <satender.gautam@fiftyfivetech.io>
2024-04-17 12:06:33 +0530

    cors chnaged

--- mappings/url_mappings.go
+++ mappings/url_mappings.go
@@ -12,7 +12,36 @@ func CreateUrlMappings() {
 	gin.SetMode(gin.ReleaseMode)
 	Router = gin.Default()
 
-	Router.Use(controllers.Cors())
+	// Middleware to enable CORS
+	Router.Use(func(c *gin.Context) {
+		// List of allowed origins
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"https://marketing-leaders.de/",
+			"http://marketing-leaders.de/",
+			"*",
+		}
+
+		origin := c.GetHeader("Origin")
+		for _, allowedOrigin := range allowedOrigins {
+			if allowedOrigin == origin {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+				c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+				// Handle preflight requests
+				if c.Request.Method == "OPTIONS" {
+					c.AbortWithStatus(204)
+					return
+				}
+			}
+		}
+
+		c.Next()
+	})
+
+	//	Router.Use(controllers.Cors())
 	// v1 of the API
 	v1 := Router.Group("/api/v1")
 	{
